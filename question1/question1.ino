d6e2248691b06870dc8f61d17e99fc83f2d0669d
Travis Smith <travissmith94@hotmail.com>
2016-02-20 19:11:36 -0500

    Controls leds and buzzer

--- question1/question1.ino
+++ question1/question1.ino
@@ -1,17 +1,41 @@
+#define MAX 1000
 void setup() {
   Serial.begin(9600);
   pinMode(3 , INPUT);
+  pinMode(13 , OUTPUT);//Controls the red LED
+  pinMode(12 , OUTPUT);//Controls the green LED
+  pinMode(7 , OUTPUT); //Controls the buzzer
 }
 
 void loop() {
-  int current = millis();
-  int sum = 0;
-  int count = 0;
-  while(millis() < current + 1000){
-    int value = analogRead(3);
-    Serial.print(value);
+  long current = millis();
+  long sum = 0;
+  long count = 0;
+  while(millis() <= current + 1000){
+    long value = analogRead(3);
+    if(value < MAX/3){
+      digitalWrite(13 , HIGH);
+    }else{
+      digitalWrite(13 , LOW);
+    }
+    
+    if(value > MAX/2){
+      digitalWrite(12 , HIGH);
+    }else{
+      digitalWrite(12 , LOW);
+    }
+    
+    if(value > MAX * (2/3.0)){
+      digitalWrite(7 , HIGH);
+    }else{
+      digitalWrite(7, LOW);
+    }
+    Serial.print(count+1);
+    Serial.print(": ");
+    Serial.println(value);
     sum += value;
     count++;
   }
-  Serial.print(sum/count);
+  Serial.print("Average: ");
+  Serial.println(sum/count);
 }
